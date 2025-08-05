61c37a0b8cf022734d8c1d66ace3e87e7f26bb58
bagjuna <juna.park124@gmail.com>
2025-08-05 22:20:44 +0900

    first commit

---
+++ src/main/java/unit/calculator/UncalApplication.java
@@ -0,0 +1,13 @@
+package unit.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UncalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UncalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unit/calculator/UncalApplicationTests.java
@@ -0,0 +1,13 @@
+package unit.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UncalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
